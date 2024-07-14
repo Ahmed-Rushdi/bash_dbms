a1fb79cdbc021c55d90722bb587305fa79a20daf
Ahmed R. El-Kilani <ahmedelkilani01@gmail.com>
2024-07-15 02:09:20 +0300

    Implemented Create Database

--- utils/create_database.sh
+++ utils/create_database.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+if [[ $# -eq 0 ]]; then
+  echo "Usage: $(basename $0) <database_name>"
+  exit 1
+elif [ -d "../databases/$1" ]; then
+  echo "Database already exists"
+  exit 1
+else
+  mkdir "../databases/$1"
+fi
