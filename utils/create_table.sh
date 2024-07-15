5e366e7b6bfdaf60e7a6bac65565874ba0d6f91d
Ahmed R. El-Kilani <ahmedelkilani01@gmail.com>
2024-07-15 17:18:18 +0300

    Implemented create table with parsing for schema | needs testing

--- utils/create_table.sh
+++ utils/create_table.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 function parse_schema() {
-    for i in "${@:3}"; do
-        #      attr=$(echo "${i}" | cut -d ":" 2> /dev/null)
-        #      WIP split attibute on ':'
-        if [[ ${#attr} -ne 2 ]]; then
-            echo "Invalid schema"
-            ./drop_table "$1" "$2"
-            return 0
+    for ((i = 3; i <= $#; i++)); do
+        if [[ ${!i} =~ ^[[:alpha:]]+:[[:alpha:]]+$ ]]; then
+            name=$(echo "${!i}" | cut -d ":" -f 1)
+            type=$(echo "${!i}" | cut -d ":" -f 2)
+            if [[ $(grep -cG "^${name}:" "../databases/${1}/.${2}.schema") -eq 0 && $type =~ ^(int|str)$ ]]; then
+                echo "${i}" >>"../databases/${1}/.${2}.schema"
+                continue
+            fi
         fi
-        echo "${i}" >>"../databases/${1}/.${2}.schema"
+        echo "Invalid schema at arg ${i}: ${!i}"
+        ./drop_table "$1" "$2"
+        return 1
     done
+    return 0
 }
-parse_schema "${@}"
+
 if [[ $# -eq 0 ]]; then
     echo "Usage: $(basename "${0}") <database_name> <table_name> <pk_name:pk_type> <attribute1_name:attribute1_type> <attribute2_name:attribute2_type>"
     exit 1
@@ -25,5 +29,5 @@ elif [[ -f "../databases/${1}/${2}" ]]; then
 else
     touch "../databases/${1}/${2}.csv"
     touch "../databases/${1}/.${2}.schema"
-    parse_schema "${@:1}"
+    parse_schema "${@}"
 fi
