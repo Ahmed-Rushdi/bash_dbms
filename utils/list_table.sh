07eb14cef6e8730a89fa36e43168a79f834d89c5
Hossam Elbatal <tokyo.hossam@gmail.com>
2024-07-15 21:25:13 +0300

    added table listing script

--- utils/list_table.sh
+++ utils/list_table.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+database="$PWD"
+
+list_tables(){
+
+    number_of_files=("$database"/*)
+
+    if [ ${#number_of_files[@]} -eq 0 ]; then
+    echo "Data base is empty"
+    fi
+
+ for file in "${number_of_files[@]}"; do
+   
+     if [[ -f $file ]]; then
+
+  
+        if [[ $file == *.csv || $file == "${file}.schema" ]]; then
+
+          echo " table : $file"
+          echo " schema: $file.schema"
+
+        fi
+    fi
+
+done
+}
+list_tables
