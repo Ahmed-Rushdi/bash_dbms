3e1b4ada58059555a4d4651451024dcd6983c828
Hossam Elbatal <tokyo.hossam@gmail.com>
2024-07-15 21:28:40 +0300

    added database listing script | to be tested

--- utils/list_database.sh
+++ utils/list_database.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+
+databases="$PWD"
+
+list_databases(){
+    echo hi
+
+  # match only directories
+  number_of_databases=("$databases"/*)
+  
+  if [[ ${#number_of_databases[@]} -eq 0 ]]; then
+
+  echo "There are no databases"
+  
+  fi
+
+  count=0;
+
+for database in "${number_of_databases[@]}"; do
+   
+    if [[ -d $database ]]; then    
+
+
+     ((count++))
+
+      echo "database $count : $database"
+
+    fi  
+
+
+done
+
+}
+
+list_databases
